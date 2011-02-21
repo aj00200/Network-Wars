35512bc921f5f3e69af7232c02eb8d5c8668ee62
AJ00200 <aj0020020@live.com>
2011-02-21 16:14:52 -0600

    Created a network folder and moved the old network file into it as classes.py

---
+++ src/network/network.py
@@ -0,0 +1,15 @@
+class Network():
+    '''Used to create a network of nodes for
+    each player inside of the game'''
+    def __init__(self):
+        self.network = {}
+
+    def check_collision(self, x, y):
+        '''Check if there is an object on
+        the (x,y) point'''
+        pass
+
+    def update(self):
+        '''Calls the balance method for each
+        node inside the network'''
+        pass
