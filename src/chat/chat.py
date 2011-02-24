dc003b12c410cc09227d45b8a7bf1bec9b5a738e
AJ00200 <aj0020020@live.com>
2011-02-24 15:51:58 -0600

    Expaned the chat API for it to be filled in by bikcmp

--- src/chat/chat.py
+++ src/chat/chat.py
@@ -2,6 +2,11 @@
 import irc
 
 def get_messages():
+    '''Get all messages from the game channel'''
     return connection.get_messages()
-def send_message():
+def send_message(message):
+    '''Sends a message to a predefined channel'''
+    pass
+def get_open_games():
+    '''Returns a list of nicks who have an open game'''
     pass
