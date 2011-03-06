59e270e249e8a4cbf0014b8c8b8902ccfae0d072
AJ00200 <aj0020020@live.com>
2011-03-06 11:23:33 -0600

    Added new functions to src/chat/chat.py

--- src/chat/chat.py
+++ src/chat/chat.py
@@ -1,6 +1,7 @@
 '''Chat interface for Network Wars'''
 import irc
 
+# Lobby Functions
 def get_messages():
     '''Get all messages from the game channel'''
     return connection.get_messages()
@@ -10,3 +11,23 @@ def send_message(message):
 def get_open_games():
     '''Returns a list of nicks who have an open game'''
     pass
+
+# Game Functions
+def start_game(nick):
+    '''Attempt to connect to nick to start a game'''
+    pass
+
+def get_game_messages():
+    '''Get all messages from a game'''
+    pass
+
+def send_game_message():
+    '''Send a message to the other player'''
+    pass
+
+def quit_game():
+    '''Tell the other player that you quit'''
+    pass
+
+def request_ip():
+    '''Request the IP and port to estabish a UDP connection'''
